93e1d780ba64029b771ed6bbe5108580aa716ca2
Alexander Pickering <alexandermpickering@gmail.com>
2015-08-10 00:39:55 +0000

    initial commit

---
+++ settingspp.cpp
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <ifstream>
+#include <ofstream>
+
---
+++ settingspp.h
@@ -0,0 +1,36 @@
+
+
+class Option
+{
+
+}
+
+class OptionNumber : public Option
+{
+
+}
+
+class OptionString : public Option
+{
+
+}
+
+class OptionNone : public Option
+{
+
+}
+
+class OptionTable : public Option
+{
+
+}
+
+class OptionArray : public Option
+{
+
+}
+
+class Settings
+{
+
+}
